40799657ab01a0c8cfd66d72aa223dfe8574eabd
Mr.Lin <Mr.Lin@DESKTOP-OGDGSHL>
2019-04-03 13:17:50 +0800

    初次提交Spring的项目模板

---
+++ src/main/java/com/gdkm/weixin/微信小程序Application.java
@@ -0,0 +1,13 @@
+package com.gdkm.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 微信小程序Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(微信小程序Application.class, args);
+	}
+
+}
