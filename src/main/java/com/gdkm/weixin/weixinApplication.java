5249c181aaa81f643d4d2183da58dbec7cc00a10
Mr.Lin <Mr.Lin@DESKTOP-OGDGSHL>
2019-04-08 11:34:04 +0800

    啊啊啊

--- src/main/java/com/gdkm/weixin/weixinApplication.java
+++ src/main/java/com/gdkm/weixin/weixinApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 微信小程序Application {
+public class weixinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(微信小程序Application.class, args);
+		SpringApplication.run(weixinApplication.class, args);
 	} 
 
 }
